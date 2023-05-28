6c0f16c6040ba7404d4fbe778d9ecd6669d85845
Shubham Prajapati <shubham.prajapati@kellogg.northwestern.edu>
2023-05-28 05:15:05 +0000

    added login for the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
+    @user = User.find_by({"email" => params["email"]})
+    if @user
+      password_the_user_typed = params["password"]
+      password_in_the_database = @user["password"]
+      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+        flash["notice"] = "Welcome"
+        redirect_to "/places/new"
+      else
+        flash["notice"] = "Our records indicate that you provided a wrong password"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "We cannot find this email id in our records"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
